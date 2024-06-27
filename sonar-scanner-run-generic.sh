e7a505ef5ae6dfc36a97d12fbdc1d827cfb97662
MattMannEC <mattmann.ec@gmail.com>
2024-06-27 14:50:40 +0200

    Project key is repo plus branch

--- sonar-scanner-run-generic.sh
+++ sonar-scanner-run-generic.sh
@@ -19,7 +19,7 @@ echo "** run sonar scanner **"
 sudo podman run \
     --rm \
     -e SONAR_HOST_URL="http://10.29.151.18:9000" \
-    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$repo" \
+    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$repo-$branch" \
     -e SONAR_TOKEN="sqa_57c8898f6b021f372215374cbf2453d05b3c8fec" \
     -v "/opt/ANEF-src/$tmp_project_root:/usr/src:z" \
     sonarsource/sonar-scanner-cli
