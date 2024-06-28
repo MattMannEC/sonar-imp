c509f5ec484a25baa2ae68b48ee6e141b2498453
MattMannEC <mattmann.ec@gmail.com>
2024-06-28 12:21:31 +0200

    Add log versionning

--- run-sonar-scanner-ihm.sh
+++ run-sonar-scanner-ihm.sh
@@ -1,4 +1,4 @@
 cd /opt/SonarQube
 sudo chown -R 1000:1000 . && 
 source ./sonar-scanner-ihm/.venv/bin/activate && 
-nohup python ./sonar-scanner-ihm/app.py > ./sonar-scanner-ihm/server.log 2>&1 &
+nohup python ./sonar-scanner-ihm/app.py > ./sonar-scanner-ihm/server$(date +'%Y%m%d_%H%M%S').log 2>&1 &
