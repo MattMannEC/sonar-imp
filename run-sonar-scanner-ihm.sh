1244db57cf7a45eb2515c33bcc87e89a9b57f789
MattMannEC <mattmann.ec@gmail.com>
2024-06-28 12:45:01 +0200

    Update logs path

--- run-sonar-scanner-ihm.sh
+++ run-sonar-scanner-ihm.sh
@@ -2,4 +2,4 @@ cd /opt/SonarQube
 sudo chown -R 1000:1000 . &&
 sudo chmod 777 -R ./logs 
 source ./sonar-scanner-ihm/.venv/bin/activate &&
-nohup python ./sonar-scanner-ihm/app.py > ./sonar-scanner-ihm/server$(date +'%Y%m%d_%H%M%S').log 2>&1 &
+nohup python ./sonar-scanner-ihm/app.py > ./logs/server$(date +'%Y%m%d_%H%M%S').log 2>&1 &
