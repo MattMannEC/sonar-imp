ca34e2ee31ee3f0f4117d8877b330c3722a95e37
MattMannEC <mattmann.ec@gmail.com>
2024-06-28 17:00:00 +0200

    update run scanner script

--- run-sonar-scanner-ihm.sh
+++ run-sonar-scanner-ihm.sh
@@ -1,4 +1,4 @@
-sudo chown -R $(whoami):"domain users" . &&
-sudo chmod 775 -R ./logs
+sudo chown -R $(whoami):"domain users" /opt/SonarQube &&
+sudo chmod 775 -R /opt/SonarQube/logs
 source ./sonar-scanner-ihm/.venv/bin/activate &&
 nohup python ./sonar-scanner-ihm/app.py > ./logs/server$(date +'%Y%m%d_%H%M%S').log 2>&1 &
